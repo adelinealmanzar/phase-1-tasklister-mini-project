11f9d07182dd33874f5162e831c32a7dbc4aca3e
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-01-17 10:24:48 -0800

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,29 +1,55 @@
-document.addEventListener("DOMContentLoaded", () => {
-  let form = document.querySelector('form')
-  let newtask = document.getElementById('new-task-description');
-  newtask.id = 'new_task_description';
-  // As a user, I should be able to click some form of a submit button
-  form.addEventListener('submit', (e) => {
-    e.preventDefault()
-    // As a user, I should be able to type a task into the input field
-    buildToDo(e.target.new_task_description.value); // e.target = form.thing with id='new_task_description'.value (typed words) will become the element of the buildToDo fn (and therefore the text content of the p)
+/*
+Deliverables
+
+// As a user, I should be able to click some form of a submit button.
+// As a user, I expect to see the task string that I provided appear 
+in the DOM after the submit button has been activated.
+*/
+
+/* HTML
+<body>
+  <div id="main-content">
+    <h1>Task Lister Lite™️</h1>
+
+    <form id="create-task-form" action="" method="POST">
+      <label for="new-task-description">Task description:</label>
+      <input type="text" id="new-task-description" name="new-task-description" placeholder="description">
+      <input type="submit" value="Create New Task">
+    </form>
+
+    <div id="list">
+      <h2>My Todos</h2>
+      <ul id="tasks">
+      </ul>
+    </div>
+*/
+
+// As a user, I should be able to type a task into the input field.
+// create show the element's value in the description box
+let form = document.querySelector('form');
+document.addEventListener('DOMContentLoaded', () => {
+  let inputTextBox = document.getElementById('new-task-description');
+  //set the new id because the old id syntax doesn't work for dot notation
+  inputTextBox.id = 'new_task_description';
+  // add sumbit event to the form
+  form.addEventListener('submit', e => {
+    //for every event, prevent the default
+    e.preventDefault();
+    // the event's target's (the form) new task value should each time be the newToDd
+    let newToDo = e.target.new_task_description.value;
+    // this newToDO should be passed in as the newToDoItem to build the todolist with
+    buildToDoList(newToDo);
     form.reset();
   })
-});
+})
 
-function buildToDo(todo) {
-  // As a user, I expect to see the task string that I 
-  // provided appear in the DOM after the submit button has been activated.
-  let p = document.createElement('p')
-  p.textContent = ` ${todo}`
-  let btn = document.createElement('button');
-  btn.addEventListener('click', handleDelete);
-  btn.textContent = 'x';
-  p.appendChild(btn);
-  let list = document.querySelector('#list')
-  list.appendChild(p)
-}
-
-function handleDelete(e) {
-  e.target.parentNode.remove() //e.target = btn
+function buildToDoList(newToDoItem) {
+  let tasksParentUl = document.querySelector('#tasks'); // grab the ul parent of li
+  let tasksChildren = document.createElement('li'); //create li for the ul parent
+  tasksChildren.textContent = `${newToDoItem}`; // each li textContent should be the newToDoItem
+  let deleteButton = document.createElement('button'); //create a new button element
+  deleteButton.textContent = 'x'; // the button's text should look like an X
+  tasksChildren.appendChild(deleteButton); // add the button element to each li child
+  deleteButton.addEventListener('click', e => e.target.parentNode.remove()) // for each delete button click, we should delete its parent (the entire todo li)
+  tasksParentUl.appendChild(tasksChildren); // add the li to the ul parent
 }
